46c47d35316a3dead7148ca396b0e6a0adfd0b74
rajmishra07 <mrrajmishra573@gmail.com>
2024-04-28 03:32:35 +0530

    added new attribute to the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,30 @@
 class User < ApplicationRecord
+  has_many :test_questions, dependent: :destroy
+  enum learner_type: { fast: 'fast', slow: 'slow' }
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :trackable, :confirmable
 
+      
+  def age
+    if dob.present?
+      now = Time.now.utc.to_date
+      dob = self.dob.to_date
+      age = now.year - dob.year - ((now.month > dob.month || (now.month == dob.month && now.day >= dob.day)) ? 0 : 1)
+      age >= 0 ? age : 0
+    else
+      0
+    end
+  end 
+  
 
   def admin?
     role == 'admin'
+  end
+
+  def name
+    "#{first_name} #{last_name}".strip
   end
   
 end
